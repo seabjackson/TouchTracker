4725e941c72b7df653fdb1c3ffd8bb3fbd2de34a
Seab <honeyboyseab@gmail.com>
2017-01-30 22:57:58 -0500

    The touch tracker can now handle multiple touch inputs to draw lines to the screen

--- TouchTracker/TouchTracker/DrawView.swift
+++ TouchTracker/TouchTracker/DrawView.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class DrawView: UIView {
-    var currentLine: Line?
+    var currentLines = [NSValue: Line]()
     var finishedLines = [Line]()
     
     func stroke(_ line: Line) {
@@ -29,13 +29,51 @@ class DrawView: UIView {
             stroke(line)
         }
         
-        if let line = currentLine {
-            // if there is a line currently being drawn, do it in red
-            UIColor.red.setStroke()
+        // draw current lines in red
+        UIColor.red.setStroke()
+        for (_, line) in currentLines {
             stroke(line)
         }
     }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        // lof statement to see the order of events
+        print(#function)
+        
+        for touch in touches {
+            let location = touch.location(in: self)
+            let newLine = Line(begin: location, end: location)
+            let key = NSValue(nonretainedObject: touch)
+            currentLines[key] = newLine
+        }
+        
+        setNeedsDisplay()
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        // log statement to see the order of events
+        print(#function)
+        
+        for touch in touches {
+            let key = NSValue(nonretainedObject: touch)
+            currentLines[key]?.end = touch.location(in: self)
+        }
+        setNeedsDisplay()
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        // log statement to see the otder of events
+        print(#function)
+        for touch in touches {
+            let key = NSValue(nonretainedObject: touch)
+            if var line = currentLines[key] {
+                line.end = touch.location(in: self)
+                finishedLines.append(line)
+                currentLines.removeValue(forKey: key)
+            }
+        }
+        setNeedsDisplay()
+    }
 }
 
 
